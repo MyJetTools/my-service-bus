0886de7e1349133624b07d439bffe2e457b8b6d7
amigin <amigin@gmail.com>
2023-04-15 00:27:13 +0300

    Removed Debug info

--- src/background/gc_timer.rs
+++ src/background/gc_timer.rs
@@ -21,21 +21,14 @@ impl MyTimerTick for GcTimer {
             let now = DateTimeAsMicroseconds::now();
             let mut topic_data = topic.get_access().await;
 
-            println!("gc_message_pages {}", topic.topic_id);
             topic_data.gc_message_pages();
 
-            println!("gc_queues_with_no_subscribers {}", topic.topic_id);
             topic_data.gc_queues_with_no_subscribers(self.app.settings.queue_gc_timeout, now);
 
-            println!("get_min_message_id {}", topic.topic_id);
             if let Some(min_message_id) = topic_data.get_min_message_id() {
                 topic_data.gc_messages(min_message_id);
             }
         }
-
-        println!("GC get_min_message_id");
         crate::operations::gc_http_connections(self.app.as_ref()).await;
-
-        println!("GC Done");
     }
 }
